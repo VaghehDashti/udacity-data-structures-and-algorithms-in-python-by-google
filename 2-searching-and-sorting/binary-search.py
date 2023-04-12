59940b68b984b85a012231978e580357e9f4dcc6
Arman Dashti <vaghehdashti@gmail.com>
2023-04-12 11:41:23 -0500

    solved binary search

--- 2-searching-and-sorting/binary-search.py
+++ 2-searching-and-sorting/binary-search.py
@@ -12,6 +12,14 @@ doesn't exist in the list."""
 
 def binary_search(input_array, value):
     """Your code goes here."""
+    mid = len(input_array)//2 - 1
+    for i in range(len(input_array)//2):  
+        if value == input_array[mid]:
+            return mid 
+        elif value > input_array[mid]:
+            mid += len(input_array[mid:])//2 - 1
+        else:
+            mid -= len(input_array[:mid])//2 - 1
     return -1
 
 test_list = [1,3,9,11,15,19,29]
