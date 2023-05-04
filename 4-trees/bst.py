d0019dd4e3f7141f94b7f5e9448ed3968b394891
Arman Dashti <vaghehdashti@gmail.com>
2023-05-04 10:15:50 -0500

    feat: completed bst

--- 4-trees/bst.py
+++ 4-trees/bst.py
@@ -9,10 +9,61 @@ class BST(object):
         self.root = Node(root)
 
     def insert(self, new_val):
-        pass
+        if not self.root:
+            self.root = Node(new_val)
+        cur_node = self.root
+        while cur_node:
+            if cur_node.value >= new_val:
+                if cur_node.left:
+                    cur_node = cur_node.left
+                else:
+                    cur_node.left = Node(new_val)
+                    break
+            else:
+                if cur_node.right:
+                    cur_node = cur_node.right
+                else:
+                    cur_node.right = Node(new_val)
+                    break
 
     def search(self, find_val):
+        cur_node = self.root
+        while cur_node:
+            if cur_node.value == find_val:
+                return True
+            elif cur_node.value >= find_val:
+                cur_node = cur_node.left
+            else:
+                cur_node = cur_node.right
         return False
+    
+    # def insert(self, new_val):
+    #         self.insert_helper(self.root, new_val)
+
+    # def insert_helper(self, current, new_val):
+    #     if current.value < new_val:
+    #         if current.right:
+    #             self.insert_helper(current.right, new_val)
+    #         else:
+    #             current.right = Node(new_val)
+    #     else:
+    #         if current.left:
+    #             self.insert_helper(current.left, new_val)
+    #         else:
+    #             current.left = Node(new_val)
+
+    # def search(self, find_val):
+    #     return self.search_helper(self.root, find_val)
+
+    # def search_helper(self, current, find_val):
+    #     if current:
+    #         if current.value == find_val:
+    #             return True
+    #         elif current.value < find_val:
+    #             return self.search_helper(current.right, find_val)
+    #         else:
+    #             return self.search_helper(current.left, find_val)
+    #     return False
     
 # Set up tree
 tree = BST(4)
